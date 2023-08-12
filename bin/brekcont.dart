86d3f7ab6b21d1c11484ac7a8ae3aec1b224f9dc
Firgo Bhaktiar Hamsah <71805833+igobhaktiar@users.noreply.github.com>
2023-08-13 05:44:11 +0700

    first commit

---
+++ bin/brekcont.dart
@@ -0,0 +1,50 @@
+import 'package:brekcont/brekcont.dart' as brekcont;
+
+void main() {
+  // bilangan prima di bawah 100
+  var primeNumbers = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    73,
+    79,
+    83,
+    89,
+    97
+  ];
+
+  var searchNumber = 13;
+
+  print('Masukkan bilangan prima diantara 1-100 : $searchNumber');
+
+  for (int i = 0; i < primeNumbers.length; i++) {
+    if (searchNumber == primeNumbers[i]) {
+      print('$searchNumber adalah bilangan prima ke-${i + 1}');
+      break;
+    }
+    print('$searchNumber != ${primeNumbers[i]}');
+  }
+
+  for (int i = 1; i <= 10; i++) {
+    if (i % 3 == 0) {
+      continue;
+    }
+    print(i);
+  }
+}
